1d18365204f367d1d423e1982520af57551c4010
InstaRobot <instarobot.net@gmail.com>
2016-07-22 02:09:26 +0300

    add empty project

---
+++ CleanApp/CleanApp/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  CleanApp
+//
+//  Created by Vitaliy Podolskiy on 22/07/2016.
+//  Copyright © 2016 InstaRobot.NET. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+  }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+  }
+
+
+}
+
---
+++ CleanApp/CleanApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanApp
+//
+//  Created by Vitaliy Podolskiy on 22/07/2016.
+//  Copyright © 2016 InstaRobot.NET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
