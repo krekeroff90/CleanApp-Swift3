a141d07c0fc429ca9c3663e23998f7103afe7f31
InstaRobot <instarobot.net@gmail.com>
2016-07-22 04:32:49 +0300

    add config

---
+++ CleanApp/CleanApp/Config.swift
@@ -0,0 +1,25 @@
+//
+//  Config.swift
+//  CleanApp
+//
+//  Created by Vitaliy Podolskiy on 22/07/2016.
+//  Copyright © 2016 InstaRobot.NET. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Config {
+  static let GoogleDataKey = "AIzaSyD4vdAksIRZU6ByLfRQ8nxYykSQ69DSVQM"
+  static let PlaylistID = "PLtovLaW_R9-PUVWIFRq_V6s4JxE_Mj2ha"
+}
+
+
+/*
+ 
+ YouTube > Data API
+ https://developers.google.com/youtube/v3/
+ 
+ 
+ 
+ */
---
+++ CleanApp/CleanApp/Helper.swift
@@ -0,0 +1,10 @@
+//
+//  Helper.swift
+//  CleanApp
+//
+//  Created by Vitaliy Podolskiy on 22/07/2016.
+//  Copyright © 2016 InstaRobot.NET. All rights reserved.
+//
+
+import Foundation
+import UIKit
