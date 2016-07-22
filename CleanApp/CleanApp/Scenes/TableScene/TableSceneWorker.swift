62a3c3e3d6d9bfa8230f199de84f851b5c9b50db
InstaRobot <instarobot.net@gmail.com>
2016-07-22 03:29:55 +0300

    conplete table scene

---
+++ CleanApp/CleanApp/Scenes/TableScene/TableSceneWorker.swift
@@ -0,0 +1,16 @@
+//
+//  TableSceneWorker.swift
+//  CleanApp
+//
+//  Created by Vitaliy Podolskiy on 22/07/2016.
+//  Copyright (c) 2016 InstaRobot.NET. All rights reserved.
+//
+
+import UIKit
+
+class TableSceneWorker {
+  // MARK: Business Logic
+  func doSomeWork() {
+    // NOTE: Do the work
+  }
+}
